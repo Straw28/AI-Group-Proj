a494c6d2c2751d8b5d70edbe99c6a35f62a5b9dc
Straw28 <mt23319@gmail.com>
2023-03-31 18:12:01 -0500

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import cityblock
-#change
+#change 2
 class Agent:
   def __init__(self, current_pos, other_pos, reward, have_block):
     self.current_pos = current_pos 
